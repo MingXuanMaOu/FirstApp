fa2e0d342e45effbd8a0839ca312a1ed211a6e9b
MingXuMaOu <92174693+MingXuMaOu@users.noreply.github.com>
2023-01-01 00:13:55 +0800

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 刘铭 on 2023/1/1.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
